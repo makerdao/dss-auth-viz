21b86e9df35c291021a617ebd4318c34af6da944
Joshua Levine <joshuapark95@gmail.com>
2018-12-05 15:20:02 -0800

    output svg

--- src/paint.js
+++ src/paint.js
@@ -1,12 +1,12 @@
-const path = require('path');
 const viz = require('viz.js');
 const opener = require('opener');
 const dot = require('graphlib-dot');
-const puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('mz/fs');
 
 const { Module, render } = require('viz.js/full.render.js');
 
-module.exports.paint = async (graph, outPath = './graph.png') => {
+module.exports.paint = async (graph, _outPath = './../graph.svg') => {
   try {
     // style
     graph.nodes().forEach(_node => {
@@ -19,32 +19,10 @@ module.exports.paint = async (graph, outPath = './graph.png') => {
       render
     }).renderString(dot.write(graph), { engine: 'circo' });
 
-    // create a headless browser instance
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({ width: 800, height: 800, deviceScaleFactor: 2 });
-    await page.goto(`file://${path.join(__dirname, '/../', 'template.htm')}`);
-
-    // render the graphic
-    await page.$eval(
-      '#container',
-      (container, _htmlString) => {
-        container.innerHTML = _htmlString;
-      },
-      htmlString
-    );
-
-    // take a screenshot
-    await page.screenshot({
-      path: outPath,
-      type: 'png',
-      fullPage: true,
-      omitBackground: true
-    });
-
-    await opener(outPath);
-
-    browser.close();
+    // write the svg tags to a file
+    const writePath = path.resolve(__dirname, _outPath);
+    fs.writeFileSync(writePath, htmlString);
+    return opener(writePath);
   } catch (err) {
     console.error(err);
   }
