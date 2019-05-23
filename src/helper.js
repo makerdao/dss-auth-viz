49d651885e202df9eab9c6184472460cbe123045
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-05-23 16:48:12 -0400

    update helper function name for better clarity

--- src/helper.js
+++ src/helper.js
@@ -4,9 +4,9 @@ const url = process.env.ETH_RPC_URL
   ? process.env.ETH_RPC_URL
   : 'http://localhost:2000';
 
-const fixCase = string => {
+const capsFLetter = string => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 module.exports.web3 = new Web3(new Web3.providers.HttpProvider(url));
-module.exports.fixCase = fixCase;
+module.exports.capsFLetter = capsFLetter;
