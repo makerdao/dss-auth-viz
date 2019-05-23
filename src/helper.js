52cfbc3ec1b1f257ce1de9f4f74616f58e8c9070
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-05-23 16:46:57 -0400

    add firstLetterCaps helper function

--- src/helper.js
+++ src/helper.js
@@ -4,4 +4,9 @@ const url = process.env.ETH_RPC_URL
   ? process.env.ETH_RPC_URL
   : 'http://localhost:2000';
 
+const fixCase = string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 module.exports.web3 = new Web3(new Web3.providers.HttpProvider(url));
+module.exports.fixCase = fixCase;
